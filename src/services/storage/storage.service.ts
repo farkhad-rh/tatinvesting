dcc90d55f395acde557af45bb01f61bb5e92a39c
Farkhad Rizvanov <space.devs@yahoo.com>
2023-01-24 17:00:31 +0500

    TAT-13 Update: storage service

--- src/services/storage/storage.service.ts
+++ src/services/storage/storage.service.ts
@@ -5,12 +5,15 @@ import type { TStorage, IStorageActions } from './storage.interface'
 export const StorageService = (type: TStorage): IStorageActions => {
   return {
     getItem: key => {
-      const item = type === 'local' ? localStorage.getItem(key) : sessionStorage.getItem(key)
+      const item =
+        type === 'local' ? localStorage.getItem(encode(key)) : sessionStorage.getItem(encode(key))
 
-      return item || null
+      return decode(item || '') || null
     },
     setItem: (key, value) => {
-      type === 'local' ? localStorage.setItem(key, value) : sessionStorage.setItem(key, value)
+      type === 'local'
+        ? localStorage.setItem(encode(key), encode(value))
+        : sessionStorage.setItem(encode(key), encode(value))
     },
     clear: () => {
       type === 'local' ? localStorage.clear() : sessionStorage.clear()
